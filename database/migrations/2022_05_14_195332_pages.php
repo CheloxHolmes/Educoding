30494db51f5eb12a249b697c5cfb14924a578b75
mcarrenoreyes <m.carrenoreyes@uandresbello.edu>
2022-05-14 16:04:12 -0400

    nuevasTablas

--- database/migrations/2022_05_14_195332_pages.php
+++ database/migrations/2022_05_14_195332_pages.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Activities extends Migration
+class Pages extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,12 @@ class Activities extends Migration
      */
     public function up()
     {
-        Schema::create('activities', function (Blueprint $table) {
+        Schema::create('pages', function (Blueprint $table) {
             $table->id();
-            $table->string('nombre')->nullable();
-            $table->string('nivel')->nullable();
-            $table->string('textoInicial')->nullable();
-            $table->string('codigo')->nullable();
-            $table->string('asignatura')->nullable();
+            $table->string('tipo')->nullable();
+            $table->string('libro')->nullable();
+            $table->string('texto')->nullable();
+            $table->string('imagen')->default("magic.webp");
             $table->timestamps();
         });
     }
@@ -31,6 +30,6 @@ class Activities extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('activities');
+        Schema::dropIfExists('pages');
     }
 }
