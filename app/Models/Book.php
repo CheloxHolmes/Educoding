6c85772306b03a4559ac0abddbe2bbd2b5669234
Kevinbriceo567 <kevinbriceo567@gmail.com>
2022-05-15 12:39:58 -0400

    Nuevo diseño para actividades

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+
+        'id',
+        'nombre',
+        'asignatura',
+        'actividad',
+        'respuesta'
+
+    ];
 }
