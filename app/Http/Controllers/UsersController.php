17f9b558f01f3f952cc7498203269f9f5ce5be0b
mcarrenoreyes <m.carrenoreyes@uandresbello.edu>
2022-08-20 18:21:45 -0400

    someDash

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use App\Models\Imagen;
+use App\Models\InventarioReim;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
@@ -34,27 +35,29 @@ class UsersController extends Controller
     public function dashboard($id)
     {
         $usuario = DB::select("SELECT * FROM usuario WHERE id = ".$id.";")[0];
-        $alumnos = User::where('tipo_usuario_id', 3)->get();
+        $alumnos = DB::select("SELECT * FROM usuario WHERE tipo_usuario_id = 3;")[0];
+        $cAlumnos = User::where('tipo_usuario_id', 3)->get();
+        $inventarioAlumno = DB::select("SELECT * FROM inventario_reim WHERE id_elemento = 900 AND sesion_id = ".$alumnos->id.";")[0];
         $imagen = DB::select("SELECT * FROM imagen WHERE nombre = '".$usuario->email."';")[0];
         //$mensajes = Mensaje::where('id_receptor', Auth::id())->get();
         //$countMensajes = count($mensajes);
         $todoUsuarios = User::all();
 
-        $cant = count($alumnos);
+        $cant = count($cAlumnos);
         $sumaCoins = 0;
-        for($i = 0; $i < count($alumnos); ++$i) {
-            $sumaCoins = $alumnos[$i]->coins+$sumaCoins;
+        for($i = 0; $i < $cant; ++$i) {
+            $sumaCoins = $inventarioAlumno->cantidad+$sumaCoins;
         }
         $sumaModulos = 0;
-        for($i = 0; $i < count($alumnos); ++$i) {
-            $sumaModulos = $alumnos[$i]->ModulosCompletados+$sumaModulos;
+        for($i = 0; $i < $cant; ++$i) {
+            $sumaModulos = $cAlumnos[$i]->ModulosCompletados+$sumaModulos;
         }
 
         $cantidadesModulosCompletadosMes = [3,7,8,1,2,3,9];
 
         return view('dashboard', [
 
-            'alumnos' => $alumnos,
+            'alumnos' => $cAlumnos,
             'cant' => $cant,
             'usuario' => $usuario,
             'avatar' => $imagen->descripcion,
