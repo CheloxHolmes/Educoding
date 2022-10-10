8224a80a24362a4c6a02733e132caa27184a85ba
mcarrenoreyes <m.carrenoreyes@uandresbello.edu>
2022-10-10 01:49:13 -0300

    chngmen1

--- resources/views/educoding.blade.php
+++ resources/views/educoding.blade.php
@@ -15,10 +15,10 @@ function sintesis(texto) {
         speechSynthesis.onvoiceschanged = voiceschanged;
         var sampleText = texto;
         var utterance = new SpeechSynthesisUtterance(sampleText);
-        utterance.lang = 'es-US';
+        utterance.lang = 'es-MX';
         utterance.pitch = 1;
         utterance.volume = 1;
-        utterance.rate = 1.1;
+        utterance.rate = 1;
         speechSynthesis.speak(utterance);
     }
 
@@ -29,7 +29,7 @@ function sintesis(texto) {
 
         sintesis(`
         ¡Hola amiguito, necesitamos tu ayuda!.
-        El pueblo de Educoding ha caído bajo hechizo en manos el mago Extrañín. Esto debido a que los habitantes de Educoding no cuidaban el medioambiente, no se educaban y no se preocupaban por su salud.
+        El pueblo de Educóding ha caído bajo hechizo en manos el mago Extrañín. Esto debido a que los habitantes de Educóding no cuidaban el medioambiente, no se educaban y no se preocupaban por su salud.
             `);
 
             sintesis(`
@@ -38,15 +38,15 @@ function sintesis(texto) {
 
 
             sintesis(`
-        ¿Cómo puedes salvar al pueblo?, muy fácil, jugando a las actividades que se encuentran en el botón, Explorar la ciudad que se encuentra en la parte superior del sitio.
+        ¿Cómo puedes salvar al pueblo?, muy fácil, jugando a las actividades que se encuentran en el botón, Explorar la ciudad, que se encuentra en la parte superior del sitio.
             `);
 
             sintesis(`
-        Recuerda además que al completar módulos podrás ganar uLearnet coins, los cuales podrás canjear en la tienda que aparece en el mismo mapa del pueblo de Educoding.
+        Recuerda además que al completar módulos podrás ganar uLearnet coins, los cuales podrás canjear en la tienda que aparece en el mismo mapa del pueblo de Educóding.
             `);
 
             sintesis(`
-        ¿Estás listo para iniciar tu aventura en Educoding?
+        ¿Estás listo para iniciar tu aventura en Educóding?
             `);
 
     };
@@ -67,13 +67,13 @@ function sintesis(texto) {
             <p>¡Hola amiguito, necesitamos tu ayuda!</p>
             <p>El pueblo de Educoding ha caído bajo hechizo en manos el mago Extrañín. Esto debido a que los habitantes de Educoding no cuidaban el medioambiente, no se educaban y no se preocupaban por su salud.</p>
             <div style="margin:5%;">
-                <img src="{{asset('assets/img/extrañin.jpg')}}" class="rounded mx-auto d-block" width="40%" height="40%">
+                <img src="https://cdn.pixabay.com/photo/2016/06/13/14/57/wizard-1454385_960_720.png" class="rounded mx-auto d-block" width="40%" height="40%">
             </div>
             <p>Necesitamos tu ayuda para restablecer el pueblo de Educoding a su normalidad, ya que los habitantes han sido convertidos en estatuas inmóviles.</p>
             <div style="margin:5%;">
                 <img src="{{asset('assets/img/mapa4.png')}}" class="rounded mx-auto d-block" width="40%" height="40%">
             </div>
-            <p><strong>¿Cómo puedes salvar al pueblo?</strong>, muy fácil, jugando a las actividades que se encuentran en el botón "<strong>Explorar la ciudad</strong>" que se encuentra en la parte superior del sitio.</p>
+            <p><strong>¿Cómo puedes salvar al pueblo?</strong>, muy fácil, jugando a las actividades que se encuentran en el botón "<strong>Explorar la ciudad</strong>", que se encuentra en la parte superior del sitio.</p>
             <p>Recuerda además que al completar módulos podrás ganar uLearnet coins, los cuales podrás canjear en la <a href="/tienda" style="color:blue" target="blank">tienda</a> que aparece en el mismo mapa del pueblo de Educoding.</p>
             <div style="margin:5%;">
                 <img src="{{asset('assets/img/coin.png')}}" class="rounded mx-auto d-block" width="40%" height="40%">
